5f037de63a4475388f4d804fdae2c73d78cae514
Xairoo <marcel.3ff8@zurreck.com>
2022-12-11 15:42:34 +0100

    docs: examples cleaned up

--- apps/web-i18n/pages/[lang]/logout.js
+++ apps/web-i18n/pages/[lang]/logout.js
@@ -3,14 +3,13 @@ import { useLogout } from "next-auth-static-site";
 import { useTranslation } from "next-i18next-static-site";
 
 export default function Logout() {
-  const { t, i18n } = useTranslation();
+  const { i18n } = useTranslation();
   const lang = i18n.language.substring(0, 2);
 
   const logout = useLogout();
 
   useEffect(() => {
     logout({
-      // redirect: false,
       apiRequest: false,
       callbackUrl: `/${lang}/logged-out`,
     });
--- apps/web/pages/logout.js
+++ apps/web/pages/logout.js
@@ -2,12 +2,10 @@ import { useEffect } from "react";
 import { useLogout } from "next-auth-static-site";
 
 export default function Logout() {
-  // const router = useRouter();
   const logout = useLogout();
 
   useEffect(() => {
     logout({
-      // redirect: false,
       apiRequest: false,
       callbackUrl: `/logged-out`,
     });
